08d5ce1286228bd2d618aac4ad6c05b36fbd60ef
magda-korzeniowska <m.korzeniowska@tlen.pl>
2022-01-27 19:12:18 +0100

    Add Error styling

--- client/src/ui/atoms/Error.jsx
+++ client/src/ui/atoms/Error.jsx
@@ -1,4 +1,7 @@
-import { Box, Typography } from '@mui/material';
+import React from 'react';
+import { Box, CardMedia, Typography } from '@mui/material';
+
+import errorImg from '../../assets/unknown_error.png';
 
 export const Error = ({ error }) => {
   return (
@@ -8,12 +11,30 @@ export const Error = ({ error }) => {
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'column',
+        minHeight: '100vh',
       }}
     >
       {
         error?.message?.includes('Network Error') ? (
           <Typography>Uruchom Server!</Typography>
-        ) : null //  TODO in TASK 1
+        ) : (
+          <Box>
+            <CardMedia
+              component="img"
+              sx={{ width: 248 }}
+              src={errorImg}
+              alt='Wystąpił nieoczekiwany błąd'
+            />
+            <Typography
+              variant='body1'
+              sx={{
+                color: 'rgba(51, 51, 51, 0.5)',
+              }}
+            >
+              Wystąpił nieoczekiwany błąd
+            </Typography>
+          </Box>
+        )
       }
     </Box>
   );

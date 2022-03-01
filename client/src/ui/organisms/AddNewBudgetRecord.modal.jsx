662af4c8d89bcf90f28b3e235e85acd354c70821
magda-korzeniowska <m.korzeniowska@tlen.pl>
2022-03-01 13:18:44 +0100

    Add minor fixes to files' paths

--- client/src/ui/organisms/AddNewBudgetRecord.modal.jsx
+++ client/src/ui/organisms/AddNewBudgetRecord.modal.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as PropTypes from 'prop-types';
 
-import { Modal } from 'ui/molecules/Modal';
+import { Modal } from '../molecules/Modal';
 
 export const AddNewBudgetRecord = ({ isOpen, handleClose }) => {
   return (

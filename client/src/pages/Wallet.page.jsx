e4001920e138cb5f04fa6765f91c67dfe1341ef6
magda-korzeniowska <m.korzeniowska@tlen.pl>
2022-04-01 20:21:29 +0200

    Add minor fixes

--- client/src/pages/Wallet.page.jsx
+++ client/src/pages/Wallet.page.jsx
@@ -5,10 +5,10 @@ import { BudgetSummary, LedgerWidget, LedgerSummary, Page } from 'ui';
 export const WalletPage = () => (
   <Page title={'Portfel'}>
     <Grid container spacing={{ xs: 3, md: 6 }}>
-      <Grid item xs={12} md={8} >
+      <Grid item xs={12} md={8}>
         <LedgerWidget />
       </Grid>
-      <Grid container item xs={12} md={4} spacing={3} >
+      <Grid container item xs={12} md={4} spacing={3}>
         <Grid item xs={12} data-test-id={'wallet-top-sidebar'}>
           <LedgerSummary />
         </Grid>

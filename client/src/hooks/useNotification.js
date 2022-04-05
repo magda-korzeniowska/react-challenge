61d6ee4260f4131ac34c8d1b88329f6dfdd1ae0d
magda-korzeniowska <m.korzeniowska@tlen.pl>
2022-04-05 11:28:33 +0200

    Add useNotification custom hook

---
+++ client/src/hooks/useNotification.js
@@ -0,0 +1,20 @@
+import { useSnackbar } from 'notistack';
+
+const snackbars = {
+  expense: { variant: 'success', message: 'Wydatek został zapisany' },
+  income: { variant: 'success', message: 'Wpływ został dodany' },
+  budget: { variant: 'success', message: 'Budżet został zdefiniowany' },
+  delete: { variant: 'success', message: 'Element został usunięty' },
+  error: { variant: 'error', message: 'Wystąpił nieoczekiwany błąd' },
+};
+
+export const useNotification = (type) => {
+  const { enqueueSnackbar } = useSnackbar();
+
+  const showSnackbar = (type) => {
+    enqueueSnackbar(snackbars[type].message, {
+      variant: snackbars[type].variant,
+    });
+  };
+  return showSnackbar;
+};
